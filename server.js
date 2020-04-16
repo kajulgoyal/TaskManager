8e6aad35373f074eb80cac51a8f8e7d1723aa87b
Kajul Goyal <kajul.goyal@nagarro.com>
2020-04-17 02:00:14 +0530

    Server port changed

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
+const PORT = process.env.PORT || 2540;
+
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/api', require('./routes/api').route)
 
-app.listen(2540, () => console.log('Server started at http://localhost:2540'))
+app.listen(PORT, () => console.log('Server started'))
